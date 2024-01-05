6ab308dce1cac41d072db48a67306ba3d5c5bef6
ImExtraPolite <imextrapolite@gmail.com>
2024-01-04 22:44:39 -0500

    add functionality to button

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const createBoard = () => {
+const createBoard = (size) => {
   const boardContainer = document.querySelector('.board');
+  removeGrid();
 
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < size; i++) {
     const createRow = document.createElement('div');
     createRow.classList.add('square-row');
     boardContainer.appendChild(createRow);
 
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < size; j++) {
       const createSquare = document.createElement('div');
       createSquare.classList.add('square');
       createRow.appendChild(createSquare);
@@ -20,9 +21,32 @@ const fillInSquare = () => {
   allSquares.forEach(square => {
     square.addEventListener('mouseover', () => {
       square.classList.add('change-color');
-    })
+    });
+  });
+}
+
+const removeGrid = () => {
+  const board = document.querySelector('.board');
+  const squareRow = document.querySelectorAll('.square-row');
+
+  squareRow.forEach(row => {
+    board.removeChild(row);
   })
 }
 
-createBoard();
-fillInSquare();
+const changeSize = () => {
+  const changeSizeButton = document.querySelector('.change-size');
+
+  changeSizeButton.addEventListener('click', () => {
+    let newSize = prompt('Enter size of grid');
+
+    if (newSize > 0 && newSize <= 100) {
+      createBoard(newSize);
+      fillInSquare();
+    } else {
+      newSize = alert('Number is too large! Enter a number between the range 1 and 101.');
+    }
+  });
+}
+
+changeSize();
